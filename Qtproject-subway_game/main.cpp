d64257121b0924a295ea1a14d038d9e0545e111f
zhb0601learner <99977902+zhb0601learner@users.noreply.github.com>
2022-05-27 20:08:15 +0800

    Add files via upload

---
+++ Qtproject-subway_game/main.cpp
@@ -0,0 +1,23 @@
+#include "mymainwindow.h"
+#include "config.h"
+#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QDebug>
+int main(int argc, char *argv[])
+{
+    FILE* in;
+    in=fopen("../Qtproject-subway_game/config.txt","r");
+    for(int i=0;i<8;i++)
+    {
+        fscanf(in,"%d",&pintu[i]);
+        qDebug()<<pintu[i];
+    }
+    fscanf(in,"%d",&state);
+    qDebug()<<state;
+    QApplication a(argc, argv);
+    MyMainWindow w;
+    w.show();
+
+    return a.exec();
+}
