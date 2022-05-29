9c941bb5ae4cb8da50d7bad898934b4077147e85
zhb0601learner <99977902+zhb0601learner@users.noreply.github.com>
2022-05-29 17:46:48 +0800

    Delete Qtproject-subway_game directory

--- Qtproject-subway_game/config.cpp
+++
@@ -1,5 +0,0 @@
-#include "config.h"
-#include <QVector>
-int pintu[8];
-int state;
-QVector<int> seq;
--- Qtproject-subway_game/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#include <QVector>
-extern int pintu[8];
-extern int state;
-extern QVector<int> seq;
-
-#endif // CONFIG_H
